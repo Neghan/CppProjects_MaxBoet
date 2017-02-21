8fec3fd5b242a2e7b1648ff27e18192084aa2a68
massimoboettesta <massimoboettesta@S3-23>
2017-02-21 10:46:51 +0100

    Correccion

--- RandomEnemyGenerator/EnemyGenerator.cpp
+++ RandomEnemyGenerator/EnemyGenerator.cpp
@@ -3,31 +3,68 @@
 #include <ctime>
 
 
-enum EnemyType{zombie,vampire,ghost,witch};
+enum class EnemyType{ZOMBIE,VAMPIRE,GHOST,WITCH,MAX	};
 
 struct Enemy{
 	EnemyType type;
 	std::string name;
 	int health;
+
+	std::string getEnemyTypeString() {
+		switch (type) {
+		case EnemyType::ZOMBIE: return "zombie";
+		case EnemyType::VAMPIRE: return "vampire";
+		case EnemyType::GHOST: return "ghost";
+		case EnemyType::WITCH: return "witch";
+		default: return "";
+		}
+	}
 };
 
-bool operator == (Enemy e1, Enemy e2) {
-	bool isTheSame = false;
-	if (e1.type == e2.type && e1.name == e2.name) {
-		isTheSame = true;
-	}
-	else {
-		isTheSame = false;
-	}
-	return isTheSame;
+bool operator == (const Enemy &e1, const Enemy &e2) {
+	return e1.name == e2.name && e1.type == e2.type;
 }
-Enemy CreateRandomEnemy() {
-	EnemyType tipo;
-	tipo = rand()%5;
-	return tipo;
+static Enemy CreateRandomEnemy() {
+	static const int MAX_LIFE{ 500 };
+	static const std::string NAMES[]{
+			"Khadgar",
+			"Muradin",
+			"Bolvar",
+			"Jaina",
+			"Medivh",
+			"Kel'Thuzad"
+	};
+	return Enemy{
+		static_cast<EnemyType>(rand() % static_cast<int>(EnemyType:: MAX)),
+		NAMES[rand() % (sizeof NAMES / sizeof std::string)],
+		rand() % MAX_LIFE
+	};
 }
 void main() {
-	srand(time(NULL));
+	srand(static_cast<unsigned>(time(nullptr)));
+	const int MAX_ENEMIES{ 5 };
+	Enemy enemies[MAX_ENEMIES];
 
-	std::string names[];
+	{
+		int i{ 0 };
+		while (i < MAX_ENEMIES) {
+			enemies[i] = CreateRandomEnemy();
+			int j{ i - 1 };
+			while (j >= 0) {
+				if (enemies[i] == enemies[j]) {
+					--i;
+					break;
+				};
+				j--;
+			};
+			++i;
+		}
+	}
+		std::cout << "List of enemies:\n";
+		for (auto &enemy : enemies) {
+			std::cout << enemy.name <<
+				"is a " << enemy.getEnemyTypeString() <<
+				"whose life is " << enemy.health << std::endl;
+		}
+		return 0;
 }
