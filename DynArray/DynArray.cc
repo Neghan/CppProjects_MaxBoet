4dfb43c371f506aa6528104cfad61c1ed8455934
massimoboettesta <massimoboettesta@S3-23>
2017-03-02 14:00:39 +0100

    DynArray

--- DynArray/DynArray.cc
+++ DynArray/DynArray.cc
@@ -1,8 +1,42 @@
 #include <ostream>
-#include "DynArray.hh"
+#include "DynArray.h"
 
 
 
-DynArray:: DynArray(void) {
+
+DynArray::DynArray(void) {
+	m_capacity = DYN_ARRAY_DEFAULT_SIZE;
+	m_size = 0;
+	m_data = new int[m_capacity]; // m_data apunta a la primera posicio de m_capacity.
+}
+
+DynArray::DynArray(int *arr, size_t size) {
+	m_capacity = size;
+	m_size = m_capacity;
+	m_data = new int[m_capacity];
+
+	for (int i = 0; i < size; i++) {
+		m_data[i] = arr[i];
+	}
+}
+
+DynArray::~DynArray(void) {
+	delete[] m_data;
+	m_data = nullptr;
+}
+
+DynArray& DynArray::operator= (const DynArray &x) {
+	DynArray z;
+	z.capacity = x.capacity;
+	z.size = x.size;
+	for (int i = 0; i < m_capacity; i++) {
+		z.m_data[i] = x.m_data[i];
+	}
 
 }
+
+int * DynArray::begin(void) const
+{
+	return m_data+m_size;
+}
+
